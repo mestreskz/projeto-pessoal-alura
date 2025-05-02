000fb627a27fc9f49ffd62436b9f2903b79d4cf4
fabricio silva <fabricionazare66@gmail.com>
2025-05-02 11:11:38 -0300

    mudanças-no-projeto

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
 'use strict'
+let titulo = document.querySelector('#titulo')
+titulo.addEventListener('click',mudarCor)
+
+function mudarCor(){
+    titulo.textContent = 'projetoAlura'
+    titulo.style.color = 'blue'
+}
+console.log(titulo)
